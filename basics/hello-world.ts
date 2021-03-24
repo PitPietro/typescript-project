9967bba6e4d04d1e3d50204bad56179177e6c9f2
Pietro Poluzzi <pit.pietro@protonmail.com>
2021-03-24 19:01:58 +0100

    basics

---
+++ basics/hello-world.ts
@@ -0,0 +1,3 @@
+console.log("Hello World");
+
+// tsc hello-world.ts && node hello-world.js && rm hello-world.js
--- basics/hello.ts
+++ basics/hello.ts
@@ -1,1 +1,7 @@
-console.log("Hello World");
+function hello(user: string, date: Date) {
+    console.log(`Hello ${user}, today is ${date}`);
+}
+
+hello("PitPietro", new Date());
+
+// tsc --noEmitOnError hello.ts && node hello.js && rm hello.js
