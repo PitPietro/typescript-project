d33b89ba1f17200fac74c79c6b6f0ae2fd656d46
Pietro Poluzzi <pit.pietro@protonmail.com>
2021-03-29 22:42:46 +0200

    jest test for the add function

--- src/code-signal-challenge/arcade/intro/add-file.ts
+++ src/code-signal-challenge/arcade/intro/add-file.ts
@@ -1,6 +1,6 @@
 // https://app.codesignal.com/arcade/intro/level-1/jwr339Kq6e3LQTsfa
 
 // returns the sum of two numbers
-function add(param1: number, param2: number): number {
+export function add(param1: number, param2: number): number {
     return param1 + param2;
 }
