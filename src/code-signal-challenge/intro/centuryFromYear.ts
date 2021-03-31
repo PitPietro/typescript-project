b2a468c2b9f0af8e6890c2e49361965b90554754
Pietro Poluzzi <pit.pietro@protonmail.com>
2021-03-31 21:59:42 +0200

    century from year

--- src/code-signal-challenge/intro/centuryFromYear.ts
+++ src/code-signal-challenge/intro/centuryFromYear.ts
@@ -1,9 +1,7 @@
-// https://app.codesignal.com/arcade/intro/level-1/egbueTZRRL5Mm4TXN
-
 /*
 given a year, return the century it is in. The first century spans from the year 1 up to and including the year
 100, the second - from the year 101 up to and including the year 200, etc.
  */
 export function centuryFromYear(year: number): number {
-    return year;
+    return Math.ceil(year / 100);
 }
