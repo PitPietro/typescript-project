6754c720ad2428f9e9ffda5a7b8ae571b32aaa02
Pietro Poluzzi <pit.pietro@protonmail.com>
2021-03-31 17:16:09 +0200

    removed .js files

--- src/basics/hello-world.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-console.log("Hello World");
-// tsc hello-world.ts && node hello-world.js && rm hello-world.js
--- src/basics/hello.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function hello(user, date) {
-    console.log("Hello " + user + ", today is " + date);
-}
-hello("PitPietro", new Date());
-// tsc --noEmitOnError hello.ts && node hello.js && rm hello.js
